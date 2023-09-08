893807bd55238ef4be47a84f338a379c412e7761
mandy0207 <mandyatwork24@gmail.com>
2023-09-07 23:41:44 -0400

    Locators

--- src/basics/SeleniumLocators.java
+++ src/basics/SeleniumLocators.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumLocators {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -19,12 +19,17 @@ public class SeleniumLocators {
 		/**
 		 * Sendkeys is used to feed any value inside WebElement 
 		 */
+		
+		
+		
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
 		driver.findElement(By.name("password")).sendKeys("secret_sauce");
 		
 		driver.findElement(By.className("submit-button")).click();
 		
-		
+		driver.findElement(By.id("react-burger-menu-btn")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("About")).click();
 
 	}
 
