6f04550607d6483f15a1560eb68b800d0435a8ea
mandy0207 <mandyatwork24@gmail.com>
2023-09-07 23:20:00 -0400

    Locators

--- src/basics/SeleniumLocators.java
+++ src/basics/SeleniumLocators.java
@@ -1,5 +1,6 @@
 package basics;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,9 +9,14 @@ public class SeleniumLocators {
 	public static void main(String[] args) {
 		
 		WebDriver driver = new ChromeDriver();
-
 		driver.manage().window().maximize();
 		driver.get("https://www.saucedemo.com/");
+		/**
+		 * Sendkeys is used to feed any value inside WebElement 
+		 */
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		
+		
 
 	}
 
