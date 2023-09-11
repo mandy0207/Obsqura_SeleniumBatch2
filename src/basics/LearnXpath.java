6684272ad7543373b5394e0a43f4d9e66db41d05
mandy0207 <mandyatwork24@gmail.com>
2023-09-10 22:55:46 -0400

    xpath

--- src/basics/LearnXpath.java
+++ src/basics/LearnXpath.java
@@ -1,10 +1,27 @@
 package basics;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LearnXpath {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
+		/**
+		 * Syntaxes of Xpaths
+		 * 1) //tagname[@attribute='value']
+		 * 2)
+		 * 
+		 * 
+		 */
 
+	WebElement loginBtn=	driver.findElement(By.xpath("//a[@class='ico-login']"));
+	loginBtn.click();
+	
 	}
 
 }
