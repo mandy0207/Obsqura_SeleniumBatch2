21c5f0236e57e7629c276a4978c5936b77cd4aaa
mandy0207 <mandyatwork24@gmail.com>
2023-09-11 23:24:47 -0400

    List concept

--- src/basics/GeneralMethods.java
+++ src/basics/GeneralMethods.java
@@ -8,17 +8,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GeneralMethods {
 
 	public static void main(String[] args) {
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.yatra.com/");
-		
-		WebElement heading = driver.findElement(By.xpath("//span[@class='main-heading']"));
+
+		WebElement heading = driver.findElement(By.xpath("//*[@class='main-heading']"));
 		/**
 		 * Grab text inside any web element using getText() Method
 		 */
-		String headingText= heading.getText();
+		String headingText = heading.getText();
 		System.out.println(headingText);
+		/**
+		 * grab tagname using below technique
+		 */
+		String tagName = heading.getTagName();
+		System.out.println("Tagname=" + tagName);
+
+		int numberofLinks = driver.findElements(By.tagName("a")).size();
+		System.out.println(numberofLinks);
 
 	}
 
