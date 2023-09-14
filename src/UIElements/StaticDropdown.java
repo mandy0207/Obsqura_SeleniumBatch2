b4b47317f8caa94e4896318ca505f16735ba5071
mandy0207 <mandyatwork24@gmail.com>
2023-09-13 22:42:58 -0400

    dropdowns

---
+++ src/UIElements/StaticDropdown.java
@@ -0,0 +1,33 @@
+package UIElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class StaticDropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://selenium.obsqurazone.com/select-input.php");
+		
+		WebElement dropdown = driver.findElement(By.id("single-input-field"));
+		
+		/**
+		 *1) First we have to create object of select class
+		 *2) Pass the dropdown webElement as an argument for select class
+		 *3) Apply various methods to select element
+		 */
+
+		Select staticDropdown = new Select(dropdown);
+		staticDropdown.selectByVisibleText("Green");
+		
+		Thread.sleep(2000);
+		staticDropdown.selectByValue("Yellow");
+		
+		
+	}
+
+}
