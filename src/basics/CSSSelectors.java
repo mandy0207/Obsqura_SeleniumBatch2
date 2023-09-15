0ec3f216fc93cab6415e0cfbc596c24adec2e296
mandy0207 <mandyatwork24@gmail.com>
2023-09-14 23:48:41 -0400

    Css Selectors

--- src/basics/CSSSelectors.java
+++ src/basics/CSSSelectors.java
@@ -10,20 +10,34 @@ public class CSSSelectors {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://demowebshop.tricentis.com/");
-		
+
 		/**
-		 * 1) If we have class attribute for any webElement css syntax would be= .className
-		 * 2) If we have Id attribute for any webElement css syntax would be= #Id
-		 * 3) If we have multiple classes for class attribute ; replace space with dot(.) (refer line 24)
-		 * 4) General sybtax for css =  Tagname[attribute='Value']
-		 *    or [attribute='Value']      (Tagname is optional)
+		 * 1) If we have class attribute for any webElement css syntax would be=
+		 * .className 
+		 * 2) If we have Id attribute for any webElement css syntax would be
+		 * hash following id= #Id 
+		 * 3) If we have multiple classes for class attribute ;
+		 * replace space with dot(.) (refer line 24) 
+		 * 4) General sybtax for css = Tagname[attribute='Value'] or [attribute='Value'] (Tagname is optional)
+		 * 
+		 * 5) Regular path syntaxt= Tagname[attribute*='value'] 
 		 */
-		
+
 		driver.findElement(By.cssSelector(".ico-login")).click();
 		driver.findElement(By.cssSelector(".email")).sendKeys("obsqura24@gmail.com");
-		driver.findElement(By.cssSelector("#Password")).sendKeys("mypassword24");
-	//	driver.findElement(By.cssSelector(".button-1.login-button")).click();
+		driver.findElement(By.cssSelector("#Password")).sendKeys("mypassword");
+		// driver.findElement(By.cssSelector(".button-1.login-button")).click();
 		driver.findElement(By.cssSelector("input[class='button-1 login-button']")).click();
+
+		/**
+		 *1)  [class='block block-category-navigation'] div:nth-child(2) ul li:nth-child(4)
+		 *2)  .block-category-navigation .list li:nth-child(4)
+		 *3)   ul[class='list'] li:nth-child(4) a
+		 */
+		driver.findElement(By.cssSelector(".block-category-navigation .list li:nth-child(4)")).click();
+		
+		String text = driver.findElement(By.cssSelector("[class*='current-item']")).getText();
+		System.out.println(text);
 		
 
 	}
