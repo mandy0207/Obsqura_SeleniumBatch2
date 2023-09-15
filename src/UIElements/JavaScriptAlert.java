f5338fd529c78fab88c294a9646a2785fbb6f545
mandy0207 <mandyatwork24@gmail.com>
2023-09-14 22:49:00 -0400

    JS alert

--- src/UIElements/JavaScriptAlert.java
+++ src/UIElements/JavaScriptAlert.java
@@ -1,11 +1,27 @@
 package UIElements;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class JavaScriptAlert {
 
 	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://selenium.obsqurazone.com/javascript-alert.php");
 		
+		driver.findElement(By.xpath("//*[@class='btn btn-success']")).click();
 		
-      driver.get("https://selenium.obsqurazone.com/javascript-alert.php");
+		/**
+		 * In order to handle any javascript based alert we have to switch into Alert Mode
+		 */
+		
+		Alert alert = driver.switchTo().alert();
+		Thread.sleep(2000);
+		alert.accept();
 
 	}
 
