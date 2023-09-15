9021e559ce94fc0dbba8fcca730b47b45061cf29
mandy0207 <mandyatwork24@gmail.com>
2023-09-14 22:45:43 -0400

    JS alert

---
+++ src/UIElements/JavaScriptAlert.java
@@ -0,0 +1,12 @@
+package UIElements;
+
+public class JavaScriptAlert {
+
+	public static void main(String[] args) {
+		
+		
+      driver.get("https://selenium.obsqurazone.com/javascript-alert.php");
+
+	}
+
+}
