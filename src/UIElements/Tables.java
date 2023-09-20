548ed6b85f85b2db8a72baea1fb2e03acebef1fe
mandy0207 <mandyatwork24@gmail.com>
2023-09-19 23:28:09 -0400

    Tables

--- src/UIElements/Tables.java
+++ src/UIElements/Tables.java
@@ -1,6 +1,10 @@
 package UIElements;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Tables {
@@ -8,7 +12,16 @@ public class Tables {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://selenium.obsqurazone.com/simple-form-demo.php");
+		driver.get("https://selenium.obsqurazone.com/table-pagination.php");
+		
+		List<WebElement> rowData = driver.findElements(By.xpath("//*[@id='dtBasicExample']//tbody//tr[7]//td"));
+		
+		for(WebElement data :rowData) {
+			System.out.println(data.getText());
+		}
+		
+		
+		
 
 	}
 
