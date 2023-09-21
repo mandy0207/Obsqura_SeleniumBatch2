33b8896cbe36e1a27b9ad366c65ded9ba5e7c77d
mandy0207 <mandyatwork24@gmail.com>
2023-09-20 23:04:06 -0400

    Dynamic Tables

--- src/UIElements/DynamicTable.java
+++ src/UIElements/DynamicTable.java
@@ -1,5 +1,4 @@
 package UIElements;
-
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -9,16 +8,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DynamicTable {
-
-	
+	static int index;
 	public static void main(String[] args) {
-       
-        
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://the-internet.herokuapp.com/tables");
       
+        List<WebElement> columns = driver.findElements(By.xpath("//*[@id='table1']/thead/tr/th"));
+        System.out.println("Columns count=: "+columns.size());
+        for(int i =0;i<columns.size();i++) {
+        	if(columns.get(i).getText().equals("Due")) {
+        		index=i+1;
+        		break;
+        	}
+        }
+        System.out.println(index);
+        
+        List<WebElement> rows = driver.findElements(By.xpath("//*[@id='table1']/tbody/tr"));
+        System.out.println("Row Count= "+rows.size());
+        for(WebElement row:rows) {
+        	System.out.println(row.findElement(By.xpath("td["+ index+"]")).getText());
+        }
       
+        driver.quit();
 	}
 	
 }
